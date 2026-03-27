efdede15c0339c4002be98f4f10af024ea093f73
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:04:36 +0200

    added a parent class GameObject

---
+++ Models/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal abstract class GameObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string Name { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+    }
+}
