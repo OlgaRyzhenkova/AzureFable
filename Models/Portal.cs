adae2b4a61ef50e8f01d0d1b30f07466cbc9abee
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:54:46 +0200

    added a class Portal, which inherits from Item

---
+++ Models/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Portal : Item
+    {
+        public Portal()
+        {
+            Name = "Portal";
+            ImagePath = "Assets/Portal.png";
+        }
+
+        public override void Interact(Hero hero)
+        {
+            if (hero.HasKey)
+            {
+                IsActive = false;
+            }
+        }
+    }
+}
