4cb740b5e8a51cec7a64317956461dd65cad4b12
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:47:28 +0200

    added an abstract class Item, which inherits from GameObject

---
+++ Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal abstract class Item : GameObject
+    {
+        public abstract void Interact(Hero hero);
+    }
+}
