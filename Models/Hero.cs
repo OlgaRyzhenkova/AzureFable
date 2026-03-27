c2d5ee51fc6cd8c32adfdf27cdf7572d277d428d
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:23:32 +0200

    added a Hero class that inherits from Unit

---
+++ Models/Hero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Hero : Unit
+    {
+        public bool HasKey { get; set; } = false;
+
+        public Hero()
+        {
+            Name = "Hero";
+            MaxHealth = 3;
+            Health = 3;
+            ImagePath = "Assets/Hero.png";
+        }
+
+        public void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+    }
+}
