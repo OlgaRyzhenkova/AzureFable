5593d849d20d29d625bef85682a6c60e412bb944
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:13:46 +0200

    added a Unit class that inherits from GameObject

---
+++ Models/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal abstract class Unit : GameObject
+    {
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            if (Health <= 0)
+            {
+                Health = 0;
+                IsActive = false;
+            }
+        }
+        public void Heal(int amount)
+        {
+            Health += amount;
+            if (Health > MaxHealth)
+            {
+                Health = MaxHealth;
+            }
+        }
+    }
+}
