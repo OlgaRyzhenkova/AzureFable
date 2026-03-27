39bf638ebe04ec232202240ccf0e51cc49a2076c
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:50:37 +0200

    added a class Key, which inherits from Item

---
+++ Models/Key.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Key : Item
+    {
+        public Key()
+        {
+            Name = "Key";
+            ImagePath = "Assets/Key.png";
+        }
+        public override void Interact(Hero hero)
+        {
+            hero.HasKey = true;
+            IsActive = false;
+        }
+    }
+}
