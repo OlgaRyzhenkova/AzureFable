114a32d57afcb96911c025404ffafeca61144f50
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 01:10:03 +0200

    added an abstract class Cell

---
+++ Models/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal abstract class Cell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Item? Item { get; set; }
+
+        protected Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Item = null;
+        }
+
+        public abstract bool IsPassable();
+    }
+}
