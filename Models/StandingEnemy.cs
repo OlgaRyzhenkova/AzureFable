37f2c545394adfa0225f2d95d66950d49ba97095
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:36:41 +0200

    added a StandingEnemy class that inherits from Enemy

---
+++ Models/StandingEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class StandingEnemy : Enemy
+    {
+        public StandingEnemy()
+        {
+            Name = "StandingEnemy";
+            ImagePath = "Assets/Enemy.png";
+            Behaviour = Enums.AIBehaviour.Standing;
+        }
+    }
+}
