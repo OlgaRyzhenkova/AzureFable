82fa01cdcd070f35eb05b1d524da6d17a9a41ed7
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 01:15:08 +0200

    added a class Wall, which inherits from Cell class.

---
+++ Models/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Wall : Cell
+    {
+        public Wall(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public override bool IsPassable()
+        {
+            return false;
+        }
+    }
+}
