d31cdc6c2fc16b6b828ec14fff506eb3ea387000
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:29:02 +0200

    added a Enemy class that inherits from Unit

---
+++ Models/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Enemy : Unit
+    {
+        public Enums.AIBehaviour Behaviour { get; set; }
+
+        public Enemy()
+        {
+            Name = "Enemy";
+            MaxHealth = 1;
+            Health = 1;
+            ImagePath = "Assets/Enemy.png";
+            Behaviour = Enums.AIBehaviour.Standing;
+        }
+    }
+}
