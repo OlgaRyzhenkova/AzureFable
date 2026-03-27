471e0d5bd01a598711ac3c2784b438f0fca774ab
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:01:15 +0200

    added class Enums

---
+++ Models/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Enums
+    {
+        public enum CellType
+        {
+            Floor,
+            Wall
+        }
+
+        public enum GameState
+        {
+            Playing,
+            Paused,
+            GameOver,
+            Win
+        }
+        public enum AIBehaviour
+        {
+            Standing,
+            Random
+        }
+    }
+}
