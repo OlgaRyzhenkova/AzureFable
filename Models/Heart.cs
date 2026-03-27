8849e789a6a93078bb9906885fc5fe9ea479286e
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:52:49 +0200

    added a class Heart, which inherits from Item

---
+++ Models/Heart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Heart : Item
+    {
+        public Heart()
+        {
+            Name = "Heart";
+            ImagePath = "Assets/Heart.png";
+        }
+
+        public override void Interact(Hero hero)
+        {
+            hero.Heal(1);
+            IsActive = false;
+        }
+    }
+}
