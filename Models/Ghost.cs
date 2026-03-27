40ea38114f0c303fcccb7d8f27e47ad32504ec1f
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 00:39:27 +0200

    added a Ghost class that inherits from Enemy

---
+++ Models/Ghost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Ghost : Enemy
+    {
+        public Ghost()
+        {
+            Name = "Ghost";
+            ImagePath = "Assets/Ghost.png";
+            Behaviour = Enums.AIBehaviour.Random;
+        }
+        public void Flee(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+    }
+}
