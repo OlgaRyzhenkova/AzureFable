f9334df847abf37bc5b1f05455996c1fe19dc932
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 01:12:00 +0200

    added a class Floor, which inherits from Cell class.

---
+++ Models/Floor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.Models
+{
+    internal class Floor : Cell
+    {
+        public Floor(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public override bool IsPassable()
+        {
+            return true;
+        }
+    }
+}
