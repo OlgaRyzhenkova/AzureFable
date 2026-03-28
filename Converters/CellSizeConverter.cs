45325bec56b3d68797880e14abfc1e9907fba3a2
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 23:10:30 +0200

    created the CellSizeConverter class in the Converters folder

---
+++ Converters/CellSizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AzureFable.Converters
+{
+    public class CellSizeConverter : IValueConverter
+    {
+        private const int CellSize = 40;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int coordinate ? coordinate * CellSize : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
