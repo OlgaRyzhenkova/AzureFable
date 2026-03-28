e0759cc70146d46b3b5b67b6a4c0c732123a3ac7
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 21:40:38 +0200

    added the MenuViewModel class

---
+++ ViewModels/MenuViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.ViewModels
+{
+    internal class MenuViewModel : ViewModelBase
+    {
+        public RelayCommand StartGameCommand { get; }
+        public RelayCommand ExitCommand { get; }
+
+        private readonly Action _onStartGame;
+
+        public MenuViewModel(Action onStartGame)
+        {
+            _onStartGame = onStartGame;
+            StartGameCommand = new RelayCommand(StartGame);
+            ExitCommand = new RelayCommand(Exit);
+        }
+
+        private void StartGame()
+        {
+            _onStartGame();
+        }
+
+        private void Exit()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
