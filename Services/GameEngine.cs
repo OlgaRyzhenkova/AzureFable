3efc0e4e519c634b0965451e43d618ab7c03cafd
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 20:57:20 +0200

    added a Game Engine class that will update everything every 0.2 seconds

---
+++ Services/GameEngine.cs
@@ -0,0 +1,65 @@
+﻿using AzureFable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace AzureFable.Services
+{
+    internal class GameEngine
+    {
+        private readonly DispatcherTimer _timer;
+        private readonly EnemyLogic _enemyLogic;
+        private readonly CollisionService _collisionService;
+        private Maze _maze;
+        private Action _onUpdate;
+
+        public GameEngine(Maze maze, Action onUpdate)
+        {
+            _maze = maze;
+            _onUpdate = onUpdate;
+            _enemyLogic = new EnemyLogic();
+            _collisionService = new CollisionService();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(200);
+            _timer.Tick += OnTick;
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            _enemyLogic.MoveGhosts(_maze.Ghosts, _maze);
+
+            bool collision = _collisionService.CheckHeroVsEnemy(_maze.Hero, _maze.Enemies);
+
+            if (!collision)
+            {
+                collision = _collisionService.CheckHeroVsGhost(_maze.Hero, _maze.Ghosts, _maze);
+            }
+
+            if (collision)
+            {
+                _collisionService.SpawnHeart(_maze);
+            }
+
+            _onUpdate();
+        }
+
+        public void UpdateMaze(Maze maze)
+        {
+            _maze = maze;
+        }
+    }
+}
