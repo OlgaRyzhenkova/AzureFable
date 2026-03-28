b5b691fe8ee7ef74d1670d9b6aa8f2aaaf0218a3
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2026-03-28 21:49:33 +0200

    added the MainViewModel class

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFable.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentView;
+
+        public ViewModelBase CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly MenuViewModel _menuViewModel;
+        private GameViewModel _gameViewModel;
+
+        public MainViewModel()
+        {
+            _menuViewModel = new MenuViewModel(StartGame);
+            _currentView = _menuViewModel;
+        }
+
+        private void StartGame()
+        {
+            _gameViewModel = new GameViewModel();
+            CurrentView = _gameViewModel;
+        }
+    }
+}
